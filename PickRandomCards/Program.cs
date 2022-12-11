eb1e7b93f42299f3aae4989930f9d6d465dd0686
逸鴻 張 <winterer32@gmail.com>
2022-12-11 21:00:10 +0800

    加入註解

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            /// 此程式碼提供使用者隨機抽取 1~13, 4 種花色的撲克牌，但不限定在同 1 副撲克牌(52張)中
+            // 提示使用者輸入想要抽取的卡片張數
             Console.Write("Enter the number of cards to pick: ");
             string? line = Console.ReadLine();
             if (int.TryParse(line, out int numberOfCards))
